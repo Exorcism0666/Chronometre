fa5aee9a9fb36b0b7a16b36be5ed1e17e79e78e6
Exorcism0666 <46537034+Exorcism0666@users.noreply.github.com>
2025-03-21 13:46:51 +0100

    Add files via upload

--- ultrason.ino
+++ ultrason.ino
@@ -1,7 +1,7 @@
 /* programme pour mesurer une distance en mm avec un HC-SR04*/
 /* numéro des broches */
-int trig = 2;
-int echo = 3;
+int trig = 9;
+int echo = 8;
 
 /* variables utilisées */
 long lecture_echo;
@@ -24,8 +24,6 @@ delayMicroseconds(10);
 digitalWrite(trig, LOW);
 lecture_echo = pulseIn(echo,HIGH);
 distance = lecture_echo*10 /58;
-if(distance>1000){digitalWrite(led1, HIGH);}
-else{digitalWrite(led1, LOW);}
 Serial.print("Distance en mm :");
 Serial.println(distance);
 delay(500);
